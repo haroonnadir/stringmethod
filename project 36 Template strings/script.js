61708bb84d195e7155861557b3ccd95705181e11
Haroon <haroonnadir143@gmail.com>
2023-09-28 19:04:20 +0500

    first commit

---
+++ project 36 Template strings/script.js
@@ -0,0 +1,6 @@
+const name = 'H@RooN N@diR';
+const age = 21;
+
+const message = `Hello, my name is ${name} and I am ${age} years old.`;
+
+console.log(message); // Output: Hello, my name is John and I am 30 years old.
